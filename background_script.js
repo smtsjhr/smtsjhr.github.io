a993b50c30d765fc2430d2249bbfc4cd85b7aa9b
Sumit Sijher <sumitsijher@gmail.com>
2020-06-10 14:19:21 -0700

    added files

---
+++ background_script.js
@@ -0,0 +1,132 @@
+const fold = 16;
+const scale = 3;
+const num_ripples = 20;
+const thickness = 1;
+
+var separation = 200;
+var space = 20;
+
+var time = 0;
+var rate = 500;
+var initial_rate = rate;
+const osc_rate = 100;
+const separation_rate = 1900;
+const space_rate = 100;
+const color_rate = 10;
+
+const alpha = 0.03;
+
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+var stop = false;
+
+var fps, fpsInterval, startTime, now, then, elapsed;
+
+
+
+startAnimating(30);
+
+
+function draw() {
+  
+  let dpr = window.devicePixelRatio || 1;
+
+  let W = canvas.style.width =  window.innerWidth;
+  let H = canvas.style.height =  window.innerHeight;
+  
+  
+  var hue = (time/color_rate + 250)%360;
+  ctx.fillStyle = `hsla(${hue}, 100%, 30%, ${alpha})`;
+  ctx.fillRect(0,0, W, H);
+  
+  separation = 200 + 100*Math.sin(time/separation_rate)**2;
+  rate = initial_rate - 20*(1 + Math.sin(time/osc_rate)**2);
+
+  ripples(ctx, 0.5*W, 0.5*H, scale*dpr, fold, separation, thickness, space, num_ripples, -time/rate);
+  
+  time += 1;
+     
+}
+
+
+
+function ripples(ctx, x_origin, y_origin, scale, fold, separation, thickness, space, ripples, time) {
+  
+  ctx.save();
+  ctx.translate(x_origin, y_origin);
+  ctx.scale(scale,scale);
+  for (let f = 0; f < fold; f++) {
+    ctx.save();
+    ctx.rotate(2*Math.PI*f/fold);
+    for (let i = 0; i < ripples; i++) {
+      ctx.save();
+      ctx.lineWidth = thickness;
+      if (i === ripples - 1 ) { 
+            line_alpha = (time%1)**2;
+      } 
+      else {
+            line_alpha = 1;
+      }
+      ctx.strokeStyle = `rgba(0,0,0, ${line_alpha})`;
+      ctx.beginPath();
+      ctx.arc(separation, 0, space*(1+i + time%1) , 0, 2*Math.PI);
+      ctx.stroke();
+      ctx.closePath();
+      ctx.restore();
+    }
+    ctx.restore();
+  }
+  ctx.restore();
+}
+
+
+function startAnimating(fps) {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  window.addEventListener("resize", checkResize, false);
+  fpsInterval = 1000 / fps;
+  then = window.performance.now();
+  startTime = then;
+  
+  animate();
+}
+
+
+function animate(newtime) {
+
+  if (stop) {
+      return;
+  }
+
+  now = newtime;
+  elapsed = now - then;
+
+  if (elapsed > fpsInterval) {
+      then = now - (elapsed % fpsInterval);
+      draw();
+  }
+  
+  requestAnimationFrame(animate);
+  
+}
+
+var checkResize = function(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+};
+
+var previousOrientation = window.orientation;
+var checkOrientation = function(){
+if(window.orientation !== previousOrientation){
+  previousOrientation = window.orientation;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+checkResize();
+};
+
+
---
+++ script.js
@@ -0,0 +1,142 @@
+const iframe_details = {
+    "RippleMoire": {
+        "url": "https://smtsjhr.github.io/RippleMoire/",
+        "codepen_url": "https://codepen.io/smtsjhr/pen/JjjGGgb",
+        "github_url": "https://github.com/smtsjhr/RippleMoire"
+    },
+    "2-ColoredLiner": {
+        "url": "https://smtsjhr.github.io/2-ColoredLiner/",
+        "codepen_url": "https://codepen.io/smtsjhr/pen/oNgdeZV",
+        "github_url": "https://github.com/smtsjhr/2-ColoredLiner"
+    },
+    "CircleWeb": {
+        "url": "https://smtsjhr.github.io/CircleWeb/",
+        "codepen_url": "https://codepen.io/smtsjhr/pen/YzyRweW",
+        "github_url": "https://github.com/smtsjhr/CircleWeb"
+    },
+    "Hall_1": {
+        "url": "https://smtsjhr.github.io/Hall_1/",
+        "codepen_url": "https://codepen.io/smtsjhr/details/LYVdweb",
+        "github_url": "https://github.com/smtsjhr/Hall_1"
+    },
+    "RecWaver": {
+        "url": "https://smtsjhr.github.io/RecWaver/",
+        "codepen_url": "https://codepen.io/smtsjhr/pen/YzyRweW",
+        "github_url": "https://github.com/smtsjhr/RecWaver"
+    },
+    "CircleWorm": {
+        "url": "https://smtsjhr.github.io/CircleWorm/",
+        "codepen_url": "https://codepen.io/smtsjhr/details/LYVrKaa",
+        "github_url": "https://github.com/smtsjhr/CircleWorm"
+    },
+    "CheckerWarp": {
+        "url": "https://smtsjhr.github.io/CheckerWarp/",
+        "codepen_url": "https://codepen.io/smtsjhr/pen/NWGNpZE",
+        "github_url": "https://github.com/smtsjhr/CheckerWarp"
+    },
+    "BubbleRing": {
+        "url": "https://smtsjhr.github.io/BubbleRing/",
+        "codepen_url": "https://codepen.io/smtsjhr/pen/BaNvEwE",
+        "github_url": "https://github.com/smtsjhr/BubbleRing"
+    },
+    "RecSlicer": {
+        "url": "https://smtsjhr.github.io/RecSlicer/",
+        "codepen_url": "https://codepen.io/smtsjhr/details/ExjEOyd",
+        "github_url": "https://github.com/smtsjhr/RecSlicer",
+        "style": "background-color: white;"
+    },
+    "FocusGrid": {
+        "url": "https://smtsjhr.github.io/FocusGrid/",
+        "codepen_url": "https://codepen.io/smtsjhr/details/ZEGLQjb",
+        "github_url": "https://github.com/smtsjhr/FocusGrid",
+        "style": "background-color: white;"
+    },
+    "FocusGrid_2": {
+        "url": "https://smtsjhr.github.io/FocusGrid_2/",
+        "codepen_url": "https://codepen.io/smtsjhr/pen/yLNvJyE",
+        "github_url": "https://github.com/smtsjhr/FocusGrid_2",
+        "style": "background-color: white;"
+    },
+    "HarmonicSpiral": {
+        "url": "https://smtsjhr.github.io/HarmonicSpiral/",
+        "codepen_url": "https://codepen.io/smtsjhr/pen/zYxjzrK",
+        "github_url": "https://github.com/smtsjhr/HarmonicSpiral",
+        "style": "background-color: white;"
+    },
+    "PixelWave": {
+        "url": "https://smtsjhr.github.io/PixelWave/",
+        "codepen_url": "https://codepen.io/smtsjhr/pen/VwYxpVw",
+        "github_url": "https://github.com/smtsjhr/PixelWave"
+    },
+    "PointChoreo": {
+        "url": "https://smtsjhr.github.io/PointChoreo/",
+        "codepen_url": "https://codepen.io/smtsjhr/pen/PoqjbYO",
+        "github_url": "https://github.com/smtsjhr/PointChoreo"
+    },
+    "PolyArch": {
+        "url": "https://smtsjhr.github.io/PolyArch/",
+        "codepen_url": "https://codepen.io/smtsjhr/pen/VwYxgZY",
+        "github_url": "https://github.com/smtsjhr/PolyArch"
+    },
+    "PolyField": {
+        "url": "https://smtsjhr.github.io/PolyField/",
+        "codepen_url": "https://codepen.io/smtsjhr/details/xxbaBVv",
+        "github_url": "https://github.com/smtsjhr/PolyField"
+    }
+};
+
+var selected_image_id;
+
+function loadCanvas(imgs) {
+    var iframe_element = document.getElementById("expanded_iframe");
+    var section_element = document.getElementById("expanded_section");
+    var title_element = document.getElementById("title");
+    var codepenURL_anchor = document.getElementById("codepenURL");
+    var githubURL_anchor = document.getElementById("githubURL");
+    var link_section_element = document.getElementById("link_section");
+    var look_text_element = document.getElementById("look_text");
+    var background_iframe_element = document.getElementById("background_iframe")
+    iframe_element.src = iframe_details[imgs.id]["url"];
+    iframe_element.style = iframe_details[imgs.id]["style"];
+    title_element.innerHTML = imgs.id;
+    codepenURL_anchor.href = iframe_details[imgs.id]["codepen_url"];
+    githubURL_anchor.href = iframe_details[imgs.id]["github_url"];
+    link_section_element.style.display = "none";
+    look_text_element.style.display = "none";
+    background_iframe_element.style.display = "none";
+    section_element.parentElement.style.display = "block";
+    image_list = document.querySelectorAll("img");
+    image_list.forEach(element => element.style = "");
+    imgs.style = "box-shadow: 0 0 40px 0px rgba(255,255,255,1)";
+}
+
+function close_canvas() {
+    var canvas_element = document.getElementById("canvas");
+    var iframe_element = document.getElementById("expanded_iframe");
+    var background_iframe_element = document.getElementById("background_iframe");
+    image_list = document.querySelectorAll("img");
+    image_list.forEach(element => element.style = "");
+    iframe_element.src = "";
+    canvas_element.style.display = "none";
+    background_iframe_element.style.display = "block";    
+}	
+
+function link_action(a) {
+    var background_iframe_element = document.getElementById("background_iframe");
+    var link_section_element = document.getElementById("link_section");
+    var canvas_container_element = document.getElementById("canvas");
+    var look_text_element = document.getElementById("look_text");
+    image_list = document.querySelectorAll("img");
+    image_list.forEach(element => element.style = "");
+    if (a.id == "links_link") {
+        canvas_container_element.style.display = "none";
+        link_section_element.style.display = "block";
+        look_text_element.style.display = "block";
+        background_iframe_element.style.display = "block";
+    } else if (a.id == "gallery_link") {
+        canvas_container_element.style.display = "none";
+        link_section_element.style.display = "none";
+        look_text_element.style.display = "none";
+        background_iframe_element.style.display = "block";
+    }
+}
