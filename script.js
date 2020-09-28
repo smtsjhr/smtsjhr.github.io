beae094d3c49df121d8a9c1231ae85de497bde97
Sumit Sijher <sumitsijher@gmail.com>
2020-09-27 19:09:20 -0700

    fetch sketchdata.json

--- script.js
+++ script.js
@@ -1,146 +1,146 @@
-const sketch_details = {
+// const sketch_details = {
 
-    "BohrGalaxy": {
-        "url": "https://smtsjhr.com/BohrGalaxy/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/JjXpoga",
-        "github_url": "https://github.com/smtsjhr/BohrGalaxy",
-    },
-    "WebIFS": {
-        "url": "https://smtsjhr.com/WebIFS/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/yLOXqpX",
-        "github_url": "https://github.com/smtsjhr/WebIFS",
-    },
-    "Z_6GaugeTile": {
-        "url": "https://smtsjhr.com/Z_6GaugeTile/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/JjXErBe",
-        "github_url": "https://github.com/smtsjhr/Z_6GaugeTile",
-        "style": "background-color: white;"
-    },
-    "MoireSun": {
-        "url": "https://smtsjhr.com/MoireSun/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/NWNqZPG",
-        "github_url": "https://github.com/smtsjhr/MoireSun",
-    },
-    "SunnyDayStarryNight": {
-        "url": "https://smtsjhr.com/SunnyDayStarryNight/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/poyRadP",
-        "github_url": "https://github.com/smtsjhr/SunnyDayStarryNight",
-    },
-    "WhispyWave": {
-        "url": "https://smtsjhr.com/WhispyWave/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/qBbmqGR",
-        "github_url": "https://github.com/smtsjhr/WhispyWave",
-        "style": "background-color: black;"
-    },
-    "RippleMoire": {
-        "url": "https://smtsjhr.com/RippleMoire/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/JjjGGgb",
-        "github_url": "https://github.com/smtsjhr/RippleMoire"
-    },
-    "2-ColoredLiner": {
-        "url": "https://smtsjhr.com/2-ColoredLiner/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/oNgdeZV",
-        "github_url": "https://github.com/smtsjhr/2-ColoredLiner"
-    },
-    "CircleWeb": {
-        "url": "https://smtsjhr.com/CircleWeb/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/YzyRweW",
-        "github_url": "https://github.com/smtsjhr/CircleWeb"
-    },
-    "Hall_1": {
-        "url": "https://smtsjhr.com/Hall_1/",
-        "codepen_url": "https://codepen.io/smtsjhr/details/LYVdweb",
-        "github_url": "https://github.com/smtsjhr/Hall_1"
-    },
-    "Hall_2": {
-        "url": "https://smtsjhr.com/Hall_2/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/XWXKGrY",
-        "github_url": "https://github.com/smtsjhr/Hall_2",
-        "style": "background-color: black;"
-    },
-    "HallTile": {
-        "url": "https://smtsjhr.com/HallTile/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/YzwWoQX",
-        "github_url": "https://github.com/smtsjhr/HallTile",
-        "style": "background-color: black;"
-    },
-    "RecWaver": {
-        "url": "https://smtsjhr.com/RecWaver/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/YzyRweW",
-        "github_url": "https://github.com/smtsjhr/RecWaver"
-    },
-    "CircleArcSine": {
-        "url": "https://smtsjhr.com/CircleArcSine/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/wvMLPyN",
-        "github_url": "https://github.com/smtsjhr/CircleArcSine"
-    },
-    "CircleFarm": {
-        "url": "https://smtsjhr.com/CircleFarm/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/eYJazdz",
-        "github_url": "https://github.com/smtsjhr/CircleFarm"
-    },
-    "CircleWorm": {
-        "url": "https://smtsjhr.com/CircleWorm/",
-        "codepen_url": "https://codepen.io/smtsjhr/details/LYVrKaa",
-        "github_url": "https://github.com/smtsjhr/CircleWorm"
-    },
-    "CheckerWarp": {
-        "url": "https://smtsjhr.com/CheckerWarp/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/NWGNpZE",
-        "github_url": "https://github.com/smtsjhr/CheckerWarp"
-    },
-    "BubbleRing": {
-        "url": "https://smtsjhr.com/BubbleRing/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/BaNvEwE",
-        "github_url": "https://github.com/smtsjhr/BubbleRing"
-    },
-    "RecSlicer": {
-        "url": "https://smtsjhr.com/RecSlicer/",
-        "codepen_url": "https://codepen.io/smtsjhr/details/ExjEOyd",
-        "github_url": "https://github.com/smtsjhr/RecSlicer",
-        "style": "background-color: white;"
-    },
-    "FocusGrid": {
-        "url": "https://smtsjhr.com/FocusGrid/",
-        "codepen_url": "https://codepen.io/smtsjhr/details/ZEGLQjb",
-        "github_url": "https://github.com/smtsjhr/FocusGrid",
-        "style": "background-color: white;"
-    },
-    "FocusGrid_2": {
-        "url": "https://smtsjhr.com/FocusGrid_2/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/yLNvJyE",
-        "github_url": "https://github.com/smtsjhr/FocusGrid_2",
-        "style": "background-color: white;"
-    },
-    "HarmonicSpiral": {
-        "url": "https://smtsjhr.com/HarmonicSpiral/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/zYxjzrK",
-        "github_url": "https://github.com/smtsjhr/HarmonicSpiral",
-        "style": "background-color: white;"
-    },
-    "PixelWave": {
-        "url": "https://smtsjhr.com/PixelWave/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/VwYxpVw",
-        "github_url": "https://github.com/smtsjhr/PixelWave"
-    },
-    "PointChoreo": {
-        "url": "https://smtsjhr.com/PointChoreo/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/PoqjbYO",
-        "github_url": "https://github.com/smtsjhr/PointChoreo"
-    },
-    "PolyArch": {
-        "url": "https://smtsjhr.com/PolyArch/",
-        "codepen_url": "https://codepen.io/smtsjhr/pen/VwYxgZY",
-        "github_url": "https://github.com/smtsjhr/PolyArch"
-    },
-    "PolyField": {
-        "url": "https://smtsjhr.com/PolyField/",
-        "codepen_url": "https://codepen.io/smtsjhr/details/xxbaBVv",
-        "github_url": "https://github.com/smtsjhr/PolyField"
-    }
-};
+//     "BohrGalaxy": {
+//         "url": "https://smtsjhr.com/BohrGalaxy/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/JjXpoga",
+//         "github_url": "https://github.com/smtsjhr/BohrGalaxy",
+//     },
+//     "WebIFS": {
+//         "url": "https://smtsjhr.com/WebIFS/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/yLOXqpX",
+//         "github_url": "https://github.com/smtsjhr/WebIFS",
+//     },
+//     "Z_6GaugeTile": {
+//         "url": "https://smtsjhr.com/Z_6GaugeTile/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/JjXErBe",
+//         "github_url": "https://github.com/smtsjhr/Z_6GaugeTile",
+//         "style": "background-color: white;"
+//     },
+//     "MoireSun": {
+//         "url": "https://smtsjhr.com/MoireSun/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/NWNqZPG",
+//         "github_url": "https://github.com/smtsjhr/MoireSun",
+//     },
+//     "SunnyDayStarryNight": {
+//         "url": "https://smtsjhr.com/SunnyDayStarryNight/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/poyRadP",
+//         "github_url": "https://github.com/smtsjhr/SunnyDayStarryNight",
+//     },
+//     "WhispyWave": {
+//         "url": "https://smtsjhr.com/WhispyWave/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/qBbmqGR",
+//         "github_url": "https://github.com/smtsjhr/WhispyWave",
+//         "style": "background-color: black;"
+//     },
+//     "RippleMoire": {
+//         "url": "https://smtsjhr.com/RippleMoire/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/JjjGGgb",
+//         "github_url": "https://github.com/smtsjhr/RippleMoire"
+//     },
+//     "2-ColoredLiner": {
+//         "url": "https://smtsjhr.com/2-ColoredLiner/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/oNgdeZV",
+//         "github_url": "https://github.com/smtsjhr/2-ColoredLiner"
+//     },
+//     "CircleWeb": {
+//         "url": "https://smtsjhr.com/CircleWeb/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/YzyRweW",
+//         "github_url": "https://github.com/smtsjhr/CircleWeb"
+//     },
+//     "Hall_1": {
+//         "url": "https://smtsjhr.com/Hall_1/",
+//         "codepen_url": "https://codepen.io/smtsjhr/details/LYVdweb",
+//         "github_url": "https://github.com/smtsjhr/Hall_1"
+//     },
+//     "Hall_2": {
+//         "url": "https://smtsjhr.com/Hall_2/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/XWXKGrY",
+//         "github_url": "https://github.com/smtsjhr/Hall_2",
+//         "style": "background-color: black;"
+//     },
+//     "HallTile": {
+//         "url": "https://smtsjhr.com/HallTile/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/YzwWoQX",
+//         "github_url": "https://github.com/smtsjhr/HallTile",
+//         "style": "background-color: black;"
+//     },
+//     "RecWaver": {
+//         "url": "https://smtsjhr.com/RecWaver/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/YzyRweW",
+//         "github_url": "https://github.com/smtsjhr/RecWaver"
+//     },
+//     "CircleArcSine": {
+//         "url": "https://smtsjhr.com/CircleArcSine/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/wvMLPyN",
+//         "github_url": "https://github.com/smtsjhr/CircleArcSine"
+//     },
+//     "CircleFarm": {
+//         "url": "https://smtsjhr.com/CircleFarm/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/eYJazdz",
+//         "github_url": "https://github.com/smtsjhr/CircleFarm"
+//     },
+//     "CircleWorm": {
+//         "url": "https://smtsjhr.com/CircleWorm/",
+//         "codepen_url": "https://codepen.io/smtsjhr/details/LYVrKaa",
+//         "github_url": "https://github.com/smtsjhr/CircleWorm"
+//     },
+//     "CheckerWarp": {
+//         "url": "https://smtsjhr.com/CheckerWarp/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/NWGNpZE",
+//         "github_url": "https://github.com/smtsjhr/CheckerWarp"
+//     },
+//     "BubbleRing": {
+//         "url": "https://smtsjhr.com/BubbleRing/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/BaNvEwE",
+//         "github_url": "https://github.com/smtsjhr/BubbleRing"
+//     },
+//     "RecSlicer": {
+//         "url": "https://smtsjhr.com/RecSlicer/",
+//         "codepen_url": "https://codepen.io/smtsjhr/details/ExjEOyd",
+//         "github_url": "https://github.com/smtsjhr/RecSlicer",
+//         "style": "background-color: white;"
+//     },
+//     "FocusGrid": {
+//         "url": "https://smtsjhr.com/FocusGrid/",
+//         "codepen_url": "https://codepen.io/smtsjhr/details/ZEGLQjb",
+//         "github_url": "https://github.com/smtsjhr/FocusGrid",
+//         "style": "background-color: white;"
+//     },
+//     "FocusGrid_2": {
+//         "url": "https://smtsjhr.com/FocusGrid_2/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/yLNvJyE",
+//         "github_url": "https://github.com/smtsjhr/FocusGrid_2",
+//         "style": "background-color: white;"
+//     },
+//     "HarmonicSpiral": {
+//         "url": "https://smtsjhr.com/HarmonicSpiral/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/zYxjzrK",
+//         "github_url": "https://github.com/smtsjhr/HarmonicSpiral",
+//         "style": "background-color: white;"
+//     },
+//     "PixelWave": {
+//         "url": "https://smtsjhr.com/PixelWave/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/VwYxpVw",
+//         "github_url": "https://github.com/smtsjhr/PixelWave"
+//     },
+//     "PointChoreo": {
+//         "url": "https://smtsjhr.com/PointChoreo/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/PoqjbYO",
+//         "github_url": "https://github.com/smtsjhr/PointChoreo"
+//     },
+//     "PolyArch": {
+//         "url": "https://smtsjhr.com/PolyArch/",
+//         "codepen_url": "https://codepen.io/smtsjhr/pen/VwYxgZY",
+//         "github_url": "https://github.com/smtsjhr/PolyArch"
+//     },
+//     "PolyField": {
+//         "url": "https://smtsjhr.com/PolyField/",
+//         "codepen_url": "https://codepen.io/smtsjhr/details/xxbaBVv",
+//         "github_url": "https://github.com/smtsjhr/PolyField"
+//     }
+// };
 
-
+var sketch_details;
 const main_body_element = document.getElementById("main_body");
 const background_iframe_element = document.getElementById("background_iframe");
 const sketch_landing = document.getElementById("sketch_landing");
@@ -160,7 +160,12 @@ const video_list = document.querySelectorAll("video");
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    hash_handler();
+    fetch('https://raw.githubusercontent.com/smtsjhr/smtsjhr.github.io/master/sketchdata.json')
+        .then(response => response.json())
+        .then(data => {sketch_details = data;})
+        .then(hash_handler());
+
+    //hash_handler();
 
     window.addEventListener("hashchange", function() {
         hash_handler();
@@ -191,6 +196,8 @@ document.addEventListener("DOMContentLoaded", function() {
         lazyVideoObserver.observe(lazyVideo);
         });
     }
+
+    
 });
 
 
